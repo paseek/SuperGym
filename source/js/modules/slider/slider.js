b1087ef65a1b6f270f3849e1a29fbef17e87df02
paseek <paseek@yahoo.com>
2023-05-11 15:34:40 +0300

    Верстаю восьмой блок - отзывы

--- source/js/modules/slider/slider.js
+++ source/js/modules/slider/slider.js
@@ -1,5 +1,7 @@
 // import Swiper from '../../vendor/swiper';
 const trainersSwiper = document.querySelector('.trainers__swiper');
+const reviewSwiper = document.querySelector('.review__swiper');
+
 
 const initTrainersSlider = () => {
   if (trainersSwiper) {
@@ -33,7 +35,46 @@ const initTrainersSlider = () => {
       })
     )();
   }
-}
+
+  if (reviewSwiper) {
+    (() =>
+      new Swiper('.review__swiper', {
+        direction: 'horizontal',
+        // loop: true,
+        slidesPerView: 1,
+        // centeredSlides: true,
+
+        navigation: {
+          nextEl: '.review__button--next',
+          prevEl: '.review__button--prev',
+        },
+
+        // breakpoints: {
+        //   // when window width is >= 320px
+        //   320: {
+        //     slidesPerView: 1,
+        //     // spaceBetween: 20,
+        //   },
+        //   // when window width is >= 768px
+        //   768: {
+        //     slidesPerView: 2,
+        //     spaceBetween: 30,
+        //   },
+        //   // when window width is >= 1199px
+        //   1199: {
+        //     slidesPerView: 4,
+        //     spaceBetween: 40,
+        //   },
+        // },
+      })
+    )();
+  }
+
+
+
+
+
+};
 
 export {initTrainersSlider};
 
