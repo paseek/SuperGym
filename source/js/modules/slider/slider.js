bde718022cedb5d5d35d66ada773757c05e6046b
paseek <paseek@yahoo.com>
2023-05-15 13:32:13 +0300

    Самотестирование - правим градиент на ios

--- source/js/modules/slider/slider.js
+++ source/js/modules/slider/slider.js
@@ -1,4 +1,4 @@
-// import Swiper from '../../vendor/swiper';
+import Swiper from '../../vendor/swiper';
 const trainersSwiper = document.querySelector('.trainers__swiper');
 const reviewSwiper = document.querySelector('.review__swiper');
 
@@ -16,17 +16,13 @@ const initTrainersSlider = () => {
         },
 
         breakpoints: {
-          // when window width is >= 320px
           320: {
             slidesPerView: 1,
-            // spaceBetween: 20,
           },
-          // when window width is >= 768px
           768: {
             slidesPerView: 2,
             spaceBetween: 30,
           },
-          // when window width is >= 1199px
           1199: {
             slidesPerView: 4,
             spaceBetween: 40,
@@ -48,82 +44,9 @@ const initTrainersSlider = () => {
           nextEl: '.review__button--next',
           prevEl: '.review__button--prev',
         },
-
-        // breakpoints: {
-        //   // when window width is >= 320px
-        //   320: {
-        //     slidesPerView: 1,
-        //     // spaceBetween: 20,
-        //   },
-        //   // when window width is >= 768px
-        //   768: {
-        //     slidesPerView: 2,
-        //     spaceBetween: 30,
-        //   },
-        //   // when window width is >= 1199px
-        //   1199: {
-        //     slidesPerView: 4,
-        //     spaceBetween: 40,
-        //   },
-        // },
       })
     )();
   }
-
-
-
-
-
 };
 
 export {initTrainersSlider};
-
-
-// export function initTrainersSwiper() {
-//   const trainersSlider = new Swiper('.trainers__slider', { // eslint-disable-line
-//     direction: 'horizontal',
-//     loop: true,
-//     spaceBetween: 33,
-//     // centeredSlides: true,
-//     grabCursor: true, // менять иконку курсора
-//     // autoplay: {
-//     //   delay: 5000,
-//     // },
-//     speed: 400,
-//     navigation: {
-//       nextEl: '.trainers__button--prev',
-//       prevEl: '.trainers__button--next',
-//     },
-//     breakpoints: {
-//       // when window width is >= 320px
-//       320: {
-//         slidesPerView: 1,
-//       },
-//       // when window width is >= 768px
-//       768: {
-//         slidesPerView: 2,
-//       },
-//       // when window width is >= 1199px
-//       1199: {
-//         slidesPerView: 4,
-//         spaceBetween: 40,
-//       },
-//     },
-//   });
-
-  // const reviewsSlider = new Swiper('.reviews__slider', {
-  //   direction: 'horizontal',
-  //   slidesPerView: 1,
-  //   spaceBetween: 33,
-  //   centeredSlides: true,
-  //   grabCursor: true, // менять иконку курсора
-  //   // autoplay: {
-  //   //   delay: 5000,
-  //   // },
-  //   // speed: 400,
-  //   navigation: {
-  //     nextEl: '.reviews__button-next',
-  //     prevEl: '.reviews__button-prev',
-  //   },
-  // });
-// }
